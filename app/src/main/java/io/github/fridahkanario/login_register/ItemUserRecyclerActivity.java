13387c40ede8b52f797e03d6f2b55f15351cba9b
fridahkanario <kanariofridah11@gmail.com>
2018-07-01 20:52:11 +0300

    Initial Commit

---
+++ app/src/main/java/io/github/fridahkanario/login_register/ItemUserRecyclerActivity.java
@@ -0,0 +1,13 @@
+package io.github.fridahkanario.login_register;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemUserRecyclerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_user_recycler);
+    }
+}
